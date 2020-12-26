f2c285857bdd5eab484edd825fb33406ca6d4913
devtoohard <e0052482@u.nus.edu>
2020-12-27 04:04:58 +0800

    map and marker

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { CustomMap } from "./CustomMap";
 import { User } from "./User";
+import { Company } from "./Company";
 
 const user = new User();
+const company = new Company();
 
-console.log(user);
+const customMap = new CustomMap("map");
+
+customMap.addUserMarker(user);
+
+customMap.addCompanyMarker(company);
