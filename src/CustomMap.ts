985d4dd949d79ba9172061e8d4b18f88fb67e264
devtoohard <e0052482@u.nus.edu>
2020-12-27 12:59:27 +0800

    model popups

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -6,6 +6,7 @@ interface Mappable {
         lat: number;
         lon: number; 
     }
+    markerContent(): string; 
 }
 
 
@@ -24,13 +25,21 @@ export class CustomMap {
 }
     
     addMarker(mappable: Mappable): void {
-        new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
-                lng: mappable.location.lon 
+                lng: mappable.location.lon
             }
-        })
+        });
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            });
+
+            infoWindow.open(this.googleMap, marker);
+        });
     }
 
 
